e3e8bd8f87b26f6c0523e52f23ca4e11382cb647
akc <akc@192.168.1.10>
2024-02-27 20:49:40 +0530

    fixed.

--- platform-core/src/main/java/com/jiddo/platform/config/RedisConfiguration.java
+++ platform-core/src/main/java/com/jiddo/platform/config/RedisConfiguration.java
@@ -7,12 +7,6 @@ import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
-
-import com.jiddo.platform.PlatformConstants;
 
 import lombok.AllArgsConstructor;
 
@@ -21,25 +15,6 @@ import lombok.AllArgsConstructor;
 public class RedisConfiguration {
 
 	private RedisConfigurationProps props;
-
-	@Bean
-	protected JedisConnectionFactory jedisConnectionFactory() {
-		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(props.getHost(),
-				props.getPort());
-		redisStandaloneConfiguration.setDatabase(props.getDb());
-		return new JedisConnectionFactory(redisStandaloneConfiguration);
-	}
-
-	@Bean(name = PlatformConstants.REDIS_TEMPLATE_FOR_UNIQUE_NUMBERS)
-	public RedisTemplate<String, Integer> redisTemplateForUnique() {
-		final RedisTemplate<String, Integer> redisTemplate = new RedisTemplate<>();
-		redisTemplate.setKeySerializer(new GenericToStringSerializer<>(String.class));
-		redisTemplate.setHashKeySerializer(new GenericToStringSerializer<>(String.class));
-		redisTemplate.setHashValueSerializer(new GenericToStringSerializer<>(String.class));
-		redisTemplate.setValueSerializer(new GenericToStringSerializer<>(String.class));
-		redisTemplate.setConnectionFactory(jedisConnectionFactory());
-		return redisTemplate;
-	}
 
 	@Bean
 	public RedissonClient getRedisson() {
