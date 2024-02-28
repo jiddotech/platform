59c47187ef5745e00e534bea0604de0e33b7a632
akc <akc@192.168.1.10>
2024-02-29 03:18:51 +0530

    fixed

--- platform-core/src/main/java/com/jiddo/platform/enums/ServiceContainerEnum.java
+++ platform-core/src/main/java/com/jiddo/platform/enums/ServiceContainerEnum.java
@@ -7,8 +7,10 @@ import lombok.Getter;
 @Getter
 public enum ServiceContainerEnum {
 
-	AUTH_SERVICE("auth"), BOOKING_SERVICE("session"), USER_SERVICE("user"), TICKET_SERVICE("ticket"),
-	HARDWARE_SERVICE("hardware"), GAME_SERVICE("game"), STORE_SERVICE("store");
+	AUTH_SERVICE("auth"), BOOKING_SERVICE("session"), EMPLOYEE_SERVICE("employee"), TICKET_SERVICE("ticket"),
+	HARDWARE_SERVICE("hardware"), ASSET_SERVICE("asset"), GAME_SERVICE("game"), STORE_SERVICE("store"),
+	GAMER_SERVICE("gamer"), PROMOTION_SERVICE("promotion"), PAYMENT_SERVICE("payment"), COMMON_SERVICE("common"),
+	NOTIFICATION_SERVICE("notification"), UD_SERVICE("ud");
 
 	private String contextPath;
 
