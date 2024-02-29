9ef1850539a9e596fc9a6adc624aa2209aa4a601
akc <akc@192.168.1.10>
2024-03-01 00:59:57 +0530

    fixed

---
+++ platform-core/src/main/java/com/jiddo/platform/dto/AuditDTO.java
@@ -0,0 +1,13 @@
+package com.jiddo.platform.dto;
+
+import java.time.Instant;
+
+import lombok.Data;
+
+@Data
+public class AuditDTO {
+	private UserDetails createdBy;
+	private UserDetails updatedBy;
+	private Instant createdAt;
+	private Instant updatedAt;
+}
