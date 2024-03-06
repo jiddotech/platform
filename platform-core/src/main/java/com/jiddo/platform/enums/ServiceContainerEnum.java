f1d862bbf682cae922e51561201c89c62e94e919
pc <pc@host.docker.internal>
2024-03-07 04:18:42 +0530

    fixed.

--- platform-core/src/main/java/com/jiddo/platform/enums/ServiceContainerEnum.java
+++ platform-core/src/main/java/com/jiddo/platform/enums/ServiceContainerEnum.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 public enum ServiceContainerEnum {
 
-	AUTH_SERVICE("auth-svc"), BOOKING_SERVICE("session-svc"), EMPLOYEE_SERVICE("employee-svc"),
+	AUTH_SERVICE("auth-svc"), BOOKING_SERVICE("session-svc"), USER_SERVICE("user-svc"),
 	TICKET_SERVICE("ticket"), HARDWARE_SERVICE("hardware-svc"), ASSET_SERVICE("asset-svc"), GAME_SERVICE("game-svc"),
 	STORE_SERVICE("store-svc"), GAMER_SERVICE("gamer-svc"), PROMOTION_SERVICE("promotion-svc"),
 	PAYMENT_SERVICE("payment-svc"), COMMON_SERVICE("common-svc"), NOTIFICATION_SERVICE("notification-svc"),
