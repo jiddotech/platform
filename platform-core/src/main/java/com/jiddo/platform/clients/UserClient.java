bc6d3b5a1acf46cd75dcd1b77a0f89130e613b9e
pc <pc@host.docker.internal>
2024-03-08 02:25:48 +0530

    fixed for userclient.

--- platform-core/src/main/java/com/jiddo/platform/clients/UserClient.java
+++ platform-core/src/main/java/com/jiddo/platform/clients/UserClient.java
@@ -2,7 +2,6 @@ package com.jiddo.platform.clients;
 
 import java.text.MessageFormat;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -15,13 +14,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jiddo.platform.PlatformConstants;
 import com.jiddo.platform.dto.UserDetails;
+import com.jiddo.platform.enums.ServiceContainerEnum;
 import com.jiddo.platform.exception.ApplicationException;
 import com.jiddo.platform.exception.PlatformExceptionCodes;
 import com.jiddo.platform.exception.ValidationException;
@@ -29,7 +28,9 @@ import com.jiddo.platform.security.SecurityConfigProps;
 import com.jiddo.platform.utility.PlatformCommonService;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -38,29 +39,18 @@ import lombok.extern.slf4j.Slf4j;
 public class UserClient {
 
 	private RestTemplate template;
-	private UrlConfig urlConfig;
 	private SecurityConfigProps securityProps;
 	private PlatformCommonService commonService;
 	private ObjectMapper mapper;
 
+	private static final ServiceContainerEnum SERVICE = ServiceContainerEnum.USER_SERVICE;
+
 	public UserDetails getUserById(String userId) {
-		if (ObjectUtils.isEmpty(userId)) {
-			return null;
-		}
-		Set<String> mobileSet = new HashSet<>();
-		mobileSet.add(userId);
-		Map<String, UserDetails> details = getUserById(mobileSet);
-		if (details.containsKey(userId)) {
-			return details.get(userId);
-		}
-		return null;
+		return getUserById(Collections.singleton(userId)).get(userId);
 	}
 
 	public UserDetails getUserByMobileNumber(String mobileNumber) {
-		Set<String> mobileNumberSet = new HashSet<>();
-		mobileNumberSet.add(mobileNumber);
-		Map<String, UserDetails> map = getUserByMobileNumber(mobileNumberSet);
-		return map.get(mobileNumber);
+		return getUserByMobileNumber(Collections.singleton(mobileNumber)).get(mobileNumber);
 	}
 
 	public Map<String, UserDetails> getUserByMobileNumber(Set<String> mobileNumbers) {
@@ -71,11 +61,10 @@ public class UserClient {
 		HttpHeaders headers = new HttpHeaders();
 		headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
 		headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-		headers.set(PlatformConstants.SSO_TOKEN_HEADER, securityProps.getCreds().get("user-service"));
+		headers.set(PlatformConstants.SSO_TOKEN_HEADER, securityProps.getCreds().get(SERVICE));
 		HttpEntity<Set<String>> entity = new HttpEntity<>(mobileNumbers, headers);
 		try {
-			String url = MessageFormat.format("{0}/user-service/secure/internal-call/user/mobileNumber",
-					urlConfig.getBaseUrl());
+			String url = commonService.getInternalCallUrl(SERVICE, "/user/mobile-number");
 			log.debug("request for fetchig user details : {} body and headers {}", url, entity);
 			ResponseEntity<JsonNode> response = template.exchange(url, HttpMethod.GET, entity, JsonNode.class);
 			return mapper.convertValue(response.getBody(), new TypeReference<Map<String, UserDetails>>() {
@@ -99,11 +88,10 @@ public class UserClient {
 		HttpHeaders headers = new HttpHeaders();
 		headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
 		headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-		headers.set(PlatformConstants.SSO_TOKEN_HEADER, securityProps.getCreds().get("user-service"));
+		headers.set(PlatformConstants.SSO_TOKEN_HEADER, securityProps.getCreds().get(SERVICE));
 		HttpEntity<Set<String>> entity = new HttpEntity<>(userIds, headers);
 		try {
-			String url = MessageFormat.format("{0}/user-service/secure/internal-server/user/id",
-					urlConfig.getBaseUrl());
+			String url = commonService.getInternalCallUrl(SERVICE, "/user/ids");
 			log.debug("request for fetchig user details : {} body and headers {}", url, entity);
 			ResponseEntity<JsonNode> response = template.exchange(url, HttpMethod.GET, entity, JsonNode.class);
 			return mapper.convertValue(response.getBody(), new TypeReference<Map<String, UserDetails>>() {
@@ -119,42 +107,18 @@ public class UserClient {
 
 	}
 
-	public UserDetails[] getAllCZOUser() {
+	public UserGetOrCreateResponse getOrCreateUser(GetOrCreateUserRequest request) {
+		if (ObjectUtils.isEmpty(request)) {
+			throw new ValidationException(PlatformExceptionCodes.INVALID_DATA.getCode(), "Invalid request");
+		}
+		log.debug("fetching or creating user with mobile :{}", request);
 		HttpHeaders headers = new HttpHeaders();
 		headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
 		headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-		headers.set(PlatformConstants.SSO_TOKEN_HEADER, securityProps.getCreds().get("user-service"));
-		HttpEntity<String> entity = new HttpEntity<>(null, headers);
-		try {
-			String url = MessageFormat.format("{0}/user-service/secure/internal-server/czo-user/all",
-					urlConfig.getBaseUrl());
-			UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url);
-
-			log.debug("request: {} headers {}", url, entity);
-			ResponseEntity<UserDetails[]> response = template.exchange(builder.toUriString(), HttpMethod.GET, entity,
-					UserDetails[].class);
-			return response.getBody();
-		} catch (HttpStatusCodeException exeption) {
-			log.error("error response from the server :{}", exeption.getResponseBodyAsString());
-			throw new ApplicationException(PlatformExceptionCodes.INTERNAL_SERVER_ERROR.getCode(),
-					"User api not working");
-		}
-	}
-
-	public UserGetOrCreateResponse getOrCreateUser(String mobileNumber) {
-		if (ObjectUtils.isEmpty(mobileNumber)) {
-			throw new ValidationException(PlatformExceptionCodes.INVALID_DATA.getCode(), "Invalid userId");
-		}
-		log.debug("fetching or creating user with mobile :{}", mobileNumber);
-		HttpHeaders headers = new HttpHeaders();
-		headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
-		headers.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-		headers.set(PlatformConstants.SSO_TOKEN_HEADER, securityProps.getCreds().get("user-service"));
-		GetOrCreateUserRequest request = new GetOrCreateUserRequest();
-		request.setMobile(mobileNumber);
+		headers.set(PlatformConstants.SSO_TOKEN_HEADER, securityProps.getCreds().get(SERVICE));
 		HttpEntity<GetOrCreateUserRequest> entity = new HttpEntity<>(request, headers);
 		try {
-			String url = MessageFormat.format("{0}/user-service/secure/internal-server/user", urlConfig.getBaseUrl());
+			String url = commonService.getInternalCallUrl(SERVICE, "/user/get-or-create");
 			log.debug("request for fetchig/creating user details : {} body and headers {}", url, entity);
 			ResponseEntity<UserGetOrCreateResponse> response = template.exchange(url, HttpMethod.POST, entity,
 					UserGetOrCreateResponse.class);
@@ -173,9 +137,13 @@ public class UserClient {
 		private boolean isNewCustomer;
 	}
 
-	@Data
+	@Builder
+	@Getter
 	public static class GetOrCreateUserRequest {
-		private String mobile;
+		private String mobileNumber;
+		private String email;
+		private String name;
+		private String actionBy;
 	}
 
 }
